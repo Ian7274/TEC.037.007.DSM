de92661c442fbdc61e29351c1fa946752255bb43
Ian7274 <inatmusel55viper@gmail.com>
2026-04-15 12:12:39 -0300

    fas

--- pokedex/app/_layout.tsx
+++ pokedex/app/_layout.tsx
@@ -1,1 +1,28 @@
- 
+ import { useEffect, useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+
+export default function App() {
+    const [pokemons, setPokemons] = useState([])
+
+    useEffect(() => {
+        buscarPokemons()
+    }, [])
+
+    async function buscarPokemons() {
+        const resposta = await fetch(
+            "https://pokeapi.co/api/v2/pokemon/?limit=10")
+            const data = await resposta.json();
+            console.log(data)
+            setPokemons(data.results);
+    }
+
+    return (
+        <ScrollView>
+            {pokemons.map((pokemon) => (
+                <View key={pokemon.name}>
+                    <Text>{pokemon.name}</Text>
+                </View>
+            ))}
+        </ScrollView>
+    )
+}
